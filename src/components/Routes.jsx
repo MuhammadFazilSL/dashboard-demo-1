b621cdf83f3eb97eb5cb1051b37033b4205588f9
MuhammadFazilSL <muhammadfazilsl455@gmail.com>
2024-03-26 21:52:23 +0530

    update build for gh-pages

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -7,15 +7,18 @@ import Customers from '../pages/Customers'
 import Products from '../pages/Products'
 import Orders from '../pages/Orders'
 import Settings from './settings/Settings'
+import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min'
 
 const Routes = () => {
     return (
         <Switch>
+            <HashRouter>
             <Route path='/customers' component={Customers}/>
             <Route path='/products' component={Products}/>
             <Route path='/orders' component={Orders}/>
             <Route path='/settings' component={Settings}/>
             <Route path='*' exact  component={Dashboard}/>
+            </HashRouter>
         </Switch>
     )
 }
